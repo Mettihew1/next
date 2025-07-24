014b9a8c419a914566cf88b0feaf26be2c91c194
Mettihew <mettihew@gmail.com>
2025-07-24 19:55:14 +0000

    asd

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,13 +1,10 @@
-type Props = {
-  params: { slug: string };
-};
 
 // ✅ Make the function async, even if you don’t use await
-export default async function NewsPage({ params }: Props) {
+export default async function NewsPage() {
   return (
     <div>
       <h1 className="text-3xl font-bold mb-4">
-        This is the slug: <span className="text-blue-600">{params.slug}</span>
+        asdf
       </h1>
     </div>
   );
