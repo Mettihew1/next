f7c02713f222345e207ddc2e5bb1adfbf524c31f
Mettihew <mettihew@gmail.com>
2025-07-26 05:41:46 +0000

    await no type

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -32,11 +32,8 @@ async function getProduct(id: string): Promise<Product | null> {
   return res.json();
 }
 
-type PageProps = {
-  params: Promise<{ id: string }>;
-};
 
-export default async function ProductPage({ params }: PageProps) {
+export default async function ProductPage({ params }: { params: { id: string } }) {
   const { id } = await params;
 
   if (!id || typeof id !== 'string') {
